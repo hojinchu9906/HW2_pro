08af408b5c7ad3fbc2d904f27ee1ae9449a4c0d4
hojinchu9906 <chjin_@naver.com>
2016-01-06 11:53:40 +0900

    성인인증

---
+++ js/hw2.js
@@ -0,0 +1,9 @@
+/**
+ * Created by sist on 2016-01-06.
+ *  힌트 : 1)클릭이벤트 처리.
+ *         2)The val() method
+ *           -returns or sets the value attribute of the selected elements.
+ *         3)if~else if문
+ *         4)alert문
+ */
+
